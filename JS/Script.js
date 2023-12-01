1b9f8c4df53b27cf1ca39ad11574a2acb22545ae
CS-Macor <123437371+CS-Macor@users.noreply.github.com>
2023-12-01 12:15:02 +0700

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -12,7 +12,7 @@ function updateClock(){
     let ampm="AM";
 
 
-     if (h > 11) {
+     if (h < 13) {
         h = h - 12; //this line is for time-formate set by taking 12h rather than 24h
         /*ampm = "PM";*/
         document.getElementById("ampm").innerText="PM";
