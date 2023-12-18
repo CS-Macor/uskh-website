18ae24ec8daba8fe537bf0f8f69e5314ea011669
CS-Macor <123437371+CS-Macor@users.noreply.github.com>
2023-12-18 16:27:18 +0700

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -26,7 +26,7 @@ function updateClock(){
         document.querySelector(".text1").style.color="white";
         document.querySelector(".text2").style.color="white";
         document.querySelector(".text3").style.color="white";
-        document.querySelector(".content p").style.color="rgb(255, 153, 0)";
+        document.querySelector(".content p").style.color= "red"; /*"rgb(255, 153, 0)";*/
 
     }
   
